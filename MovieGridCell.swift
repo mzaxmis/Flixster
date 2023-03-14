f9f776d6446a65e2219c199504e27a40df3c1fe1
mzaxmis <121317176+mzaxmis@users.noreply.github.com>
2023-03-14 00:57:50 -0400

    Create MovieGridCell.swift

---
+++ MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+// MovieGridCell.swift
+// Flixster
+//
+// Created by Misbah M on 3/13/23.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+
+    
+    
+}
